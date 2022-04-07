4546819cdd56a1bcf4ff51791f18264d17ca9a94
Vincent NOURY <vincent.noury@efrei.net>
2022-04-07 01:54:30 -0700

    Added reconnecting

--- script/messaging.py
+++ script/messaging.py
@@ -19,10 +19,14 @@ class Messenger:
         self.ws = websocket.WebSocket()
         self.message = ""
         self.channelId = ""
+        self.message_id = ""
+        self.guild_id = ""
         self.author = {'username' : 'VTBot',
                         'id' : "948058941668069386"}
         self.attachments = []
-        self.log = logger.log()
+        self.log = logger.log
+        self.seq = 0
+        self.session_id = ""
 
     def get_message(self):
         message = self.ws.recv()
@@ -50,6 +54,8 @@ class Messenger:
         self.ws.send(json.dumps(heartbeat_JSON))
         self.log('debug', "Heartbeat sent")
 
+
+
     def getAuthorUsername(self):
         return self.author['username']
 
@@ -65,15 +71,26 @@ class Messenger:
     def getAttachments(self):
         return self.attachments
 
+    def getMessageId(self):
+        return self.message_id
+
+    def getGuildId(self):
+        return self.guild_id
+
     def get_all_infos(self, event):
-        self.author = event['d']['author']
-        self.message = event['d']['content']
-        self.channelId = event['d']['channel_id']
-        self.attachments = event['d']['attachments']
-        self.log('debug', f"{self.author['username']} : {self.message}")
+        if event['t'] == "MESSAGE_CREATE":
+            self.author = event['d']['author']
+            self.message = event['d']['content']
+            self.channelId = event['d']['channel_id']
+            self.attachments = event['d']['attachments']
+            self.message_id = event['d']['id']
+            self.guild_id = event['d']['guild_id']
+            self.log('debug', f"{self.author['username']} : {self.message}")
+        if event['s'] != None:
+            self.seq = event['s']
 
     def connect(self):
-        sys.stdout.write("## Connecting ##\n")
+        self.log('none', "Connecting")
         self.ws.connect(self.url)
         event = self.get_message()
         self.heartbeat_interval = event['d']['heartbeat_interval'] / 1000
@@ -82,32 +99,26 @@ class Messenger:
         self.log('none', "Connected")
 
     def reconnect(self):
-        pass
-
-
-    def core(self):
-        while True:
-            try:
-                event = self.get_message()
-            except:
-                self.send_message(self.channelId, "Help, I'm dying")
-                sys.stdout.write("Help, I'm dying\n")
-                self.reconnect()
-            try :
-                self.get_all_infos(event)
-            except:
-                pass
-
-    def op_code_treatment(self, event):
-        op_code = event['op']
-        if op_code == 11:
-            self.log("debug", "heartbeat recieved")
-        elif op_code == 1:
-            self.send_heartbeat()
-
-    def start(self):
+        self.log("debug", "Reconnecting")
+        self.ws.close()
+        seld.log("trace", "Connectiong closed")
         self.connect()
 
+        payload = {
+          "op": 6,
+          "d": {
+            "token": self.TOKEN,
+            "session_id": self.session_id,
+            "seq": self.seq
+          }
+        }
+
+        self.ws.send(json.dumps(heartbeat_JSON))
+        message = self.get_message()
+        self.op_code_treatment(message)
+        self.get_all_infos(event)
+
+    def identify(self):
         payload = {
           "op": 2,
           "d": {
@@ -122,4 +133,47 @@ class Messenger:
         }
 
         self.ws.send(json.dumps(payload))
+        message = self.get_message()
+        self.op_code_treatment(message)
+        self.get_all_infos(message)
+
+    def core(self):
+        while True:
+            try:
+                event = self.get_message()
+            except:
+                self.send_message(self.channelId, "Help, I'm dying")
+                self.log('debug', "Help, I'm dying")
+                self.reconnect()
+            try :
+                self.op_code_treatment(event)
+                self.get_all_infos(event)
+            except:
+                pass
+
+    def op_code_treatment(self, event): # A faire get_all_infos from op_code_treatment
+        op_code = event['op']
+        if op_code == 11:
+            self.log("debug", "heartbeat recieved")
+            self.log('trace', f"event : {event}")
+        elif op_code == 9:
+            self.log('debug', "Re-identifying")
+            self.log('trace', f"event : {event}")
+            self.identify()
+        elif op_code == 0:
+            if event['t'] == "GUILD_CREATE":
+                pass
+            elif event['t'] == "READY":
+                self.session_id = event['d']['session_id']
+                self.log('trace', f"event : {event}")
+        elif op_code == 1:
+            self.send_heartbeat()
+            self.log('trace', f"event : {event}")
+        self.get_all_infos(event)
+
+    def start(self):
+        self.connect()
+
+        self.identify()
+
         threading._start_new_thread(self.core, ())
